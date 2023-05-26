ed4fd332e0189bb3320c0c1d8e845a8513683e2d
sameer <febsec14@gmail.com>
2023-05-26 15:08:23 +0545

    optimizing folder struct

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require('../models/ProductModel')
+const ProductModel = require('../models/Product')
 const upload = require('../middlewares/uploadMiddleware');
 
 
@@ -8,7 +8,7 @@ exports.createProduct = async (req, res) => {
     const { productName, price } = req.body;
     const imageName = req.file.filename; // Extract the filename from the uploaded file
 
-    const newProduct = await Product.create({ productName, price, imageName });
+    const newProduct = await ProductModel.create({ productName, price, imageName });
 
     res.status(201).json(newProduct);
   } catch (error) {
@@ -20,11 +20,11 @@ exports.createProduct = async (req, res) => {
 
 // GET product
 exports.showProducts = async (req, res) => {
-    try {
-        const newProduct = await Product.find()
-        res.status(201).json(newProduct);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' })
-    }
+  try {
+    const newProduct = await ProductModel.find()
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 }
--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const productController = require('../controllers/productController')
+const productController = require('../controllers/ProductController')
 const upload = require('../middlewares/uploadMiddleware')
 const router = express.Router()
 
