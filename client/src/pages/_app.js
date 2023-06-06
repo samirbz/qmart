85d9aeace64b83fba214c9c7a53f65abd34b8deb
sameer <febsec14@gmail.com>
2023-06-06 18:41:50 +0545

    project optimized

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import Nav from './components/Nav';
+import Nav from '@/components/Nav';
 
 export default function App({ Component, pageProps }) {
   return (
